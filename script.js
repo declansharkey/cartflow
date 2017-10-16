8f03ad4acae81b9527e367f0db7f734ac4c05af8
Jinn Wang <jrwang@pnimedia.com>
2017-10-16 16:42:45 -0700

    add view details content, add Find btn animation

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
     $(".club-select button").click(function () {
+
         if ($('.clubs-list').hasClass('hidden')) {
-            $('.clubs-list').removeClass("hidden");
+            setTimeout(function () {
+                TweenMax.set($('.clubs-list'), { className: "-=hidden" });
+                TweenMax.fromTo($('.clubs-list'), 0.3, { y: -20, opacity: 0, maxHeight: '0' }, { y: 0, opacity: 1, maxHeight: '800px' })
+            }, 500);
+
+        } else {
+            var tl = new TimelineMax();
+            tl.to($('.clubs-list'), 0.3, { y: -20, opacity: 0, maxHeight: '0' }, 0);
+            tl.set($('.clubs-list'), { className: "+=hidden" }, 0.3);
         }
     });
 });
