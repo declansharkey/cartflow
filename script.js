cb2fdd0663d19b181d1a66e991a98190bfac9bf7
Jinn Wang <jrwang@pnimedia.com>
2017-10-17 16:57:31 -0700

    update Clubs section styling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
                 TweenMax.set($('.clubs-list'), { className: "-=hidden" });
                 TweenMax.fromTo($('.clubs-list'), 0.3, { y: -10, opacity: 0 }, { y: 0, opacity: 1 });
                 TweenLite.set($('.clubs-list'), { height: 'auto' });
-                TweenLite.from($('.clubs-list'), 0.3, { height:0 });
+                TweenLite.from($('.clubs-list'), 0.3, { height: 0 });
             }, 0);
 
         } else {
@@ -22,17 +22,22 @@ $(document).ready(function () {
         });
     });
 
+    // View Detials Button
     $('.view-details-btn').click(function() {
         var viewDetails = $(this).parent().siblings('.view-details');
         ('.view-details');
         if (viewDetails.hasClass('hidden')) {
-            TweenMax.fromTo($(viewDetails), 0.3, { y: -20, maxHeight: 0, opacity: 0 }, { y: 0, maxHeight: '70px', opacity: 1 });
+            TweenMax.fromTo($(viewDetails), 0.3, { y: -20, opacity: 0 }, { y: 0, opacity: 1 });
+            TweenLite.set($(viewDetails), { height: 'auto' });
+            TweenLite.from($(viewDetails), 0.3, { height: 0 });
             TweenMax.set($(viewDetails), { className: "-=hidden" });
+            $(this).text('- Hide details');
         }
         else {
             var tl = new TimelineMax();
-            tl.to($(viewDetails), 0.3, { y: -20, maxHeight: 0, opacity: 0}, 0);
+            tl.to($(viewDetails), 0.3, { y: -20, height: 0, opacity: 0}, 0);
             tl.set($(viewDetails), { className: "+=hidden" }, 0.3);
+            $(this).text('+ View details');
         }
     })
 });
