14eaf3428542f64bbae65ced7e09d8435634a00e
Jinn Wang <jrwang@pnimedia.com>
2017-10-17 14:33:57 -0700

    update auto height animation

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
         if ($('.clubs-list').hasClass('hidden')) {
             setTimeout(function () {
                 TweenMax.set($('.clubs-list'), { className: "-=hidden" });
-                TweenMax.fromTo($('.clubs-list'), 0.3, { y: -20, opacity: 0, maxHeight: '0' }, { y: 0, opacity: 1, maxHeight: '400px' })
+                TweenMax.fromTo($('.clubs-list'), 0.3, { y: -10, opacity: 0 }, { y: 0, opacity: 1 });
+                TweenLite.set($('.clubs-list'), { height: 'auto' });
+                TweenLite.from($('.clubs-list'), 0.3, { height:0 });
             }, 0);
 
         } else {
             var tl = new TimelineMax();
-            tl.to($('.clubs-list'), 0.3, { y: -20, opacity: 0, maxHeight: '0' }, 0);
+            tl.to($('.clubs-list'), 0.3, { y: -20, opacity: 0, height: 0 }, 0);
             tl.set($('.clubs-list'), { className: "+=hidden" }, 0.3);
         }
     });
